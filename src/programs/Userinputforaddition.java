8dae3e4a80fc334747fe42e13be6b9e20b789e86
shabanaazmeen <azmeen222@gmail.com>
2025-07-28 15:14:24 +0530

    first commit

---
+++ src/programs/Userinputforaddition.java
@@ -0,0 +1,20 @@
+package programs;
+import java.util.Scanner;
+
+public class Userinputforaddition {
+
+	public static void main(String[] args) {
+		Scanner num=new Scanner(System.in);
+		System.out.println("enter the value a");
+		int a=num.nextInt();
+		System.out.println("enter the number 2");
+		int b=num.nextInt();
+		System.out.println("addition" +(a+b));
+		System.out.println("subtraction" +(a-b));
+		System.out.println("multiplication"+(a*b));
+		System.out.println("division" +(a/b));
+		System.out.println("modulus" +(a%b));
+		
+	}
+
+}
